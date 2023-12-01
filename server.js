e30994e754dfd2f173a317c2e709067c3d4be41e
qotibamhamed <qotibaeiad98@gmail.com>
2023-12-01 14:04:42 +0200

    fix add user

--- server.js
+++ server.js
@@ -48,51 +48,25 @@ io.on('connection', (socket) => {
     console.log(`Client connected with ID ${clientId}`);
 
     socket.on('addUser', async (userData) => {
-        const newUser = new User({
-                    name: userData.name,
-                    phoneNumber: userData.phoneNumber,
-                });
-        if (userData && userData.name && userData.phoneNumber) {
-            try {
                 
-                const newUser = new User({
-                    name: userData.name,
-                    phoneNumber: userData.phoneNumber,
-                });
-                // Save the new user to MongoDB
-                await newUser.save();
-    
-                console.log(`User added to MongoDB: ${JSON.stringify(newUser)}`);
-    
-                // Emit a success message back to the client
-                socket.emit('userAdded', 'User added to MongoDB successfully');
-            } catch (error) {
-                console.error('Error adding user to MongoDB:', error);
-                // Emit an error message back to the client
-                socket.emit('userAddedError', 'Error adding user to MongoDB');
-            }
-        } else {
-            // Emit an error message back to the client if 'userData' is not formatted correctly
-            socket.emit('userAddedError', 'Invalid user data');
+    const newUser = new User({
+        name: userData.name,
+        phoneNumber: userData.phoneNumber,
+    });
+    // Save the new user to MongoDB
+    await newUser.save();
+
+    console.log(`User added to MongoDB: ${JSON.stringify(newUser)}`);
+
+            // Emit a success message back to the client
+            socket.emit('userAdded', 'User added to MongoDB successfully');
+        } catch (error) {
+            console.error('Error adding user to MongoDB:', error);
+            // Emit an error message back to the client
+            socket.emit('userAddedError', 'Error adding user to MongoDB');
         }
-    });
-
-    // ... (rest of your socket event handlers)
-
-    socket.on('disconnect', () => {
-        console.log(`Client with ID ${clientId} disconnected`);
-        var client1 = { id: clientId, time: new Date().getDate() };
-        console.log(client1);
-    });
-});
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, '0.0.0.0', () => {
-    console.log('Hi, I am the server');
-    console.log(`Server running at http://0.0.0.0:${PORT}`);
-});
-
-function generateUniqueId() {
-    return Math.random().toString(36).substr(2, 9);
+    } else {
+        // Emit an error message back to the client if 'userData' is not formatted correctly
+        socket.emit('userAddedError', 'Invalid user data');
+    }
 }
